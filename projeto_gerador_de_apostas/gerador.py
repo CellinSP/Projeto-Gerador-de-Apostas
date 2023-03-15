09f03f1fec5143fc4d489dd3fc4f223b42409b5e
Marcelo Henrique Vieira Amendola <negociosmh1@gmail.com>
2023-03-15 13:23:58 -0300

    adicionando_funções_p1

--- projeto_gerador_de_apostas/gerador.py
+++ projeto_gerador_de_apostas/gerador.py
@@ -1,8 +1,10 @@
 from random import randint
 armazenamento = list()
-for n in range(1, 61):
+usuario = int(input('Quantos números você gostaria de gerar? '))
+for n in range(1, usuario + 1):
     gerador = randint(1, 61)
     if gerador in armazenamento:
         gerador = randint(1, 61)
     else:
         armazenamento.append(gerador)
+print(armazenamento)
