4c74d7ef45873fe8157105afbcea807796e70a16
Marcelo Henrique Vieira Amendola <negociosmh1@gmail.com>
2023-03-16 17:04:19 -0300

    novas_funções_p2

--- projeto_gerador_de_apostas/gerador.py
+++ projeto_gerador_de_apostas/gerador.py
@@ -1,10 +1,20 @@
 from random import randint
+
+arquivo = open('historico.txt', 'r', encoding='utf-8')
+conteudo = arquivo.read()
+print(conteudo)
+historico = list()
 armazenamento = list()
+
 usuario = int(input('Quantos números você gostaria de gerar? '))
+
 for n in range(1, usuario + 1):
     gerador = randint(1, 61)
-    if gerador in armazenamento:
+    while gerador in armazenamento:
         gerador = randint(1, 61)
-    else:
-        armazenamento.append(gerador)
+    armazenamento.append(gerador)
+    arquivo = open('historico.txt', 'a', encoding='utf-8')
+    arquivo.write((str(gerador) + ' '))
+arquivo.write('\n')
+arquivo.close()
 print(armazenamento)
